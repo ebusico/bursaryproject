075ad4da63b88e31ddc321967b01dafeb95e82f2
ChiZhangCZ <chi.zhang@qa.com>
2019-03-28 12:41:01 +0000

    Hello World with fetch

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const bodyParser= require('body-parser')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+app.use(bodyParser.urlencoded({extended: true}))
+
+var db
+
+MongoClient.connect('mongodb://localhost:27017/sampleDB', { useNewUrlParser: true }, (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('sampleDB') // whatever your database name is
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
 })
 
-app.listen(3000, function() {
-  console.log('listening on 3000')
+app.get('/', (req, res) => {
+  db.collection('hello').find().toArray(function(err, results) {
+    res.send(results[0].string)
+  })
 })
