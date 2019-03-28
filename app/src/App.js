e48e33020f3baae7c4b626d529a837e34253e454
KrisDuff456 <41322885+KrisDuff456@users.noreply.github.com>
2019-03-28 15:40:08 +0000

    added react app and connected with Express

--- app/src/App.js
+++ app/src/App.js
@@ -30,8 +30,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-database">{this.state.data}</h1>
           </header>
+          <h1 className="App-database">{this.state.data}</h1>
+          
       </div>
     );
   }
