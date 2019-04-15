3fe1fa9ba89e5c3e238ccc0c6f5ee756decfa53c
Eihern <ernest.anthony94@gmail.com>
2019-04-15 19:22:02 +0100

    search filter added

--- React/src/components/list-trainee.component.js
+++ React/src/components/list-trainee.component.js
@@ -19,8 +19,13 @@ export default class ListTrainee extends Component {
     
     constructor(props) {
         super(props);
-                
-        this.state = {trainees: []};
+                 
+       //Added searchString: "" - Ernie
+
+        this.state = {trainees: [], searchString: ""};
+        
+       //Added onChangeSearch - Ernie
+        this.onChangeSearch = this.onChangeSearch.bind(this);
     }
     
     componentDidMount() {
@@ -41,16 +46,62 @@ export default class ListTrainee extends Component {
                 console.log(error);
             })
     }
-    
-    traineeList() {
-        return this.state.trainees.map(function(currentTrainee, i){
-            return <Trainee trainee={currentTrainee} key={i} />;
-        })
+
+    // Added onChangeSearch(e) function. Needed for the search filter - Ernie
+    onChangeSearch(e) {
+        this.setState({
+            searchString: e.target.value
+        });
+    }
+
+
+    traineeList(search) {
+
+        let trainees = this.state.trainees;
+
+        // return this.state.trainees.map(function(currentTrainee, i){
+
+        
+            if (search.length > 0) {
+                this.state.trainees.filter(function (i) {
+                    return i.trainee_fname.toLowerCase().match(search)
+                    // if (i.trainee_fname.toLowerCase().match(search)){
+                    //     return <Trainee trainee={i} key={i} />; 
+                    // }
+                });
+            }
+
+            // return <Trainee trainee={currentTrainee} key={i} />;
+        // })
     }
     
+
+    
+
     render() {
+
+        //Declared variables in order to read input from search function
+        
+        let trainees = this.state.trainees;
+        let search = this.state.searchString.trim().toLowerCase();
+        
+        if (search.length > 0) {
+            trainees = trainees.filter(function (i) {
+                return i.trainee_fname.toLowerCase().match(search)
+            });
+        }
+
         return (
             <div>
+                
+                {/* Actual search function - Ernie */}
+                <input
+                    type="text"
+                    value={this.state.searchString}
+                    onChange={this.onChangeSearch}
+                    placeholder="Find trainee..."
+                />
+
                 <h3>Trainees List</h3>
                 <Link to={"/create"}>Add Trainee</Link>
                 <table className="table table-striped" style={{ marginTop: 20 }} >
@@ -62,9 +113,31 @@ export default class ListTrainee extends Component {
                             <th>Action</th>
                         </tr>
                     </thead>
+
+
+                    {/* Commented this for now, show list of trainees with all details and delete button */}
+                    {/* <tbody>
+                        { this.traineeList(search) }
+                    </tbody> */}
+
+
+                    {/* Shows list of trainees that are from the database - Ernie */}
+                    
                     <tbody>
-                        { this.traineeList() }
+                        {trainees.map(t => {
+                            return (
+                                <tr>
+                                    <td> {t.trainee_fname}</td>
+                                    <td> {t.trainee_lname}</td>
+                                    <td> {t.trainee_email}</td>
+                                    <td> 
+                                        <button onClick={()=>axios.get('http://localhost:4000/trainee/delete/'+t._id).then((response) => window.location.reload())}>Delete</button>
+                                    </td>
+                                </tr>
+                            );
+                        })}
                     </tbody>
+
                 </table>
             </div>
         )
