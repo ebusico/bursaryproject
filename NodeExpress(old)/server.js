36cddd56120e5f3162074fd6875106376b8ccfef
Bibek1 <bibek.gurung1@nhs.net>
2019-05-16 17:07:59 +0100

    testing

---
+++ NodeExpress(old)/server.js
@@ -0,0 +1,32 @@
+var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const PORT = 4000;
+
+var cookieParser = require('cookie-parser');
+
+
+require('dotenv').config()
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+mongoose.connect('mongodb://localhost:27017/trainees', { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
+
+app.use('/trainee', require('./routes/trainee_routes'));
+app.use('/admin', require('./routes/admin_routes'));
+app.use('/auth', require('./routes/auth_routes'));
+
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
+})
