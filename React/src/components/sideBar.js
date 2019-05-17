e3556a6a2cd70037c33b853d1ec63bb6a0e24be6
KrisDuff456 <41322885+KrisDuff456@users.noreply.github.com>
2019-05-17 09:40:42 +0100

    added an example pdf for sidebar

--- React/src/components/sideBar.js
+++ React/src/components/sideBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { slide as Menu } from "react-burger-menu";
+import pdf_example1 from './helper/example_1.pdf';
 
 export default class sideBar extends React.Component {
 	 showSettings (event) {
@@ -10,8 +11,7 @@ export default class sideBar extends React.Component {
     // Pass on our props
      <Menu>
 	 <h1>Help Guide</h1>
-        <a id="home" className="menu-item" href="/">Home</a>
-        
+        <a id="Example pdf 1" target="_new" className="menu-item" href={pdf_example1}>Example 1</a>
       </Menu>
   );
 };
