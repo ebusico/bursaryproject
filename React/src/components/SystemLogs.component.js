006fba9863ea8f4d6cfa5783caebd1c359ebde7f
KrisDuff456 <41322885+KrisDuff456@users.noreply.github.com>
2019-05-22 18:32:33 +0100

    added logging and react side

--- React/src/components/SystemLogs.component.js
+++ React/src/components/SystemLogs.component.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import fileReader from 'react-file-reader'; 
 import { render } from 'react-dom';
+
 import logs from './helper/server_logs.txt';
 import { authService } from './modules/authService';
+import AccessDenied from './modules/AccessDenied';
 import '../css/system_logs.css';
 
 export default class SystemLogs extends Component {
@@ -13,16 +15,37 @@ export default class SystemLogs extends Component {
 					logs:logs,
 					currentUser: authService.currentUserValue,
 					};
-					console.log(this.state.logs);
 			}
-	
+			
+		download (){
+		/*
+			var FileSaver = require('file-saver');
+			const response = {
+				file: './helper/server_logs.txt'
+			};
+			var blob = new Blob([response.file], {type: "text/plain;charset=utf-8"});
+			FileSaver.saveAs(blob, "server_logs.txt");
+			
+			<button  className='system_btn' onClick={this.download}>Download Logs</button>
+		
+			window.open(response.file);
+			*/
+		}
+		
 	render() {
-		return(
+		if(this.state.currentUser.token.role === 'admin'){
+			return(
 			<div className="system_wrapper">
 				<div className="system_body">
-					<iframe src={this.state.logs} frameBorder="0"/>
+					<iframe id="serverLogs" src={this.state.logs} frameBorder="0"/>
 				</div>
 			</div>
 			)
+		}else{
+			return (
+				<AccessDenied/>
+			);
+		
 		}
+	}
 }
--- React/src/components/sideBar.component.js
+++ React/src/components/sideBar.component.js
@@ -1,17 +1,43 @@
 import React from "react";
+import { authService } from './modules/authService';
 import { slide as Menu } from "react-burger-menu";
 
 
 export default class sideBar extends React.Component {
+	constructor(props) {
+        super(props);
+			this.state = {
+				show_server_logs: false,
+				currentUser: authService.currentUserValue
+		}
+	}
+	componentDidMount(){
+		if(this.state.currentUser.token.role === 'admin'){
+			this.setState({
+				show_server_logs: true,
+			})
+		}else{
+			this.setState({
+				show_server_logs: false
+			})
+		}
+	}
 	 showSettings (event) {
 		event.preventDefault();
 		}
    render () {
-	   
+	const {show_server_logs} = this.state;
+	
 	return (
 		// Pass on our props
 		<Menu>
+			<a onClick={() => { document.location.href = "/"; }}>Home</a>
 			<a id="HelperGuide" target="_new" className="menu-item" href="https://docs.google.com/document/d/1AXQ9NMtyfb5IkY0sDhafANRjIISliqCThlpj8kq99LA/edit">User Guide</a>
+			{show_server_logs ? 
+				<a onClick={() => { document.location.href = "/system_logs"; }}>
+				System Logs
+				</a>
+				: ""}
 		</Menu>
 		);
 	};
