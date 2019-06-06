f92dfacbbaec77b1a83b4ab7bf5228f18271afbe
Daniel Smith <danielsmith.2796@googlemail.com>
2019-06-06 13:18:43 +0100

    imrpoved appearance of edit-dates component

--- React/src/components/edit-dates.component.js
+++ React/src/components/edit-dates.component.js
@@ -273,7 +273,8 @@ export default class EditDates extends Component {
 
                     <Collapse in={this.state.open}>
                     <div className="form-group">
-                        <label>Bursary Amount</label>
+                        <label>Bursary Amount:</label>
+                        <br />
 						&nbsp;&nbsp;
                                 <input 
                                     type="number"
