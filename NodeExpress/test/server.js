d45d68d9cb65c1180cbe7b9dceeac1aeffbcaa82
VLukovski <47025947+VLukovski@users.noreply.github.com>
2019-06-10 12:18:51 +0100

    Update server.js
    most of the trainee route tests

--- NodeExpress/test/server.js
+++ NodeExpress/test/server.js
@@ -42,29 +42,33 @@ describe('trainee/', () => {
 
 // MOCK DATA
 let addUser = {
-	'trainee_email': 'testing@aol.co.uk',
+	'trainee_email': 'test@test.test',
 	'trainee_password': 'password123',
-	'trainee_start_date': "2019-06-01T00:00:00.000",
-	'trainee_end_date': "2019-09-01T00:00:00.000",
-	'added_By': 'Adam',
+	'trainee_start_date': "Wed May 01 2019 12:00:00 GMT+0100 (GMT Summer Time)",
+	'trainee_end_date': "Wed Jul 24 2019 00:00:00 GMT+0100 (GMT Summer Time)",
 	'trainee_fname': 'John',
 	'trainee_lname': 'Adams',
-	'bursary_amount': '30',
-	'bursary': 'true',
-	'trainee_bench_start_date': "2019-09-02T00:00:00.000",
-	'trainee_bench_end_date': "2019-12-02T00:00:00.000",
+	'trainee_bursary_amount': '30',
+	'bursary': 'True',
+	'trainee_bench_start_date': "Thur Jun 25 2019",
+	'trainee_bench_end_date': "Wed Jul 31 2019",
 	'trainee_days_worked': '1',
 	'bank_holiday': 'true'
 };
 
-
-let deleteUser = {
-	'trainee_email': 'delete@aol.co.uk',
+let addUser2 = {
+	'trainee_email': 'test2@test.test',
 	'trainee_password': 'password123',
-	'trainee_start_date': '21-01-2019',
-	'trainee_end_date': '25-04-2019',
-	'trainee_fname': 'DeleteJohn',
+	'trainee_start_date': "Wed May 01 2019 12:00:00 GMT+0100 (GMT Summer Time)",
+	'trainee_end_date': "Wed Jun 24 2019 00:00:00 GMT+0100 (GMT Summer Time)",
+	'trainee_fname': 'John',
 	'trainee_lname': 'Adams',
+	'trainee_bursary_amount': '0',
+	'bursary': 'False',
+	'trainee_bench_start_date': "Thur Jun 25 2019",
+	'trainee_bench_end_date': "Wed Aug 31 2019",
+	'trainee_days_worked': '1',
+	'bank_holiday': 'true'
 };
 
 let adminUser = {
@@ -76,15 +80,26 @@ let newPassword = {
 }
 
 let updateUser = {
-	'trainee_email': 'testing@aol.co.uk',
+	'trainee_email': 'test@test.test',
 	'trainee_password': 'password123',
 	'trainee_fname': 'John',
 	'trainee_lname': 'Adams',
 	'trainee_account_no': '19982350',
 	'trainee_sort_code': '090921',
+	'trainee_bank_name': 'Bank'
 };
 
+let updateDates = {
+	'trainee_start_date': "Wed May 01 2019 12:00:00 GMT+0100 (GMT Summer Time)",
+	'trainee_end_date': "Wed Jul 24 2019 00:00:00 GMT+0100 (GMT Summer Time)",
+	'trainee_bench_start_date': "Thur Jun 25 2019",
+	'trainee_bench_end_date': "Wed Aug 31 2019"
+}
 
+let updateBursary = {
+	'trainee_bursary_amount': '50',
+	'bursary': 'true'
+}
 
 describe('trainee tests', () => {
 	var userId;
@@ -119,6 +134,26 @@ describe('trainee tests', () => {
 			 	 throw new Error(`Expected 200 but got ${res}. error is ${err}`);
 			 }
 		});
+//		chai.request('http://localhost:4000').post('/trainee/add').set('content-type', 'application/json').send(addUser2).end((err, res) => {
+//			 if (res.status == '200'){
+//			 	console.log('Account has been created');
+//				chai.request('http://localhost:4000').get('/trainee/').end((err, res) => {
+//					let i = 0;
+//						while(i < res.body.length){
+//							if(res.body[i].trainee_email == addUser2.trainee_email){
+//								console.log("email : "+ res.body[i].trainee_email);
+//								console.log("id : "+res.body[i]._id);
+//								userId = res.body[i]._id;
+//								userEmail = res.body[i].trainee_email;
+//								break;
+//							}
+//						i++;
+//					}
+//					chai.request('http://localhost:4000').post('/trainee/daysToWork').send({'trainee_email': userEmail}).end((err, res) => {
+//					});
+//				});
+//			 }
+//		});
 	});
 //	after((done) => {
 //		chai.request('http://localhost:4000').get('/trainee/').end((err, res) => {
@@ -169,6 +204,66 @@ describe('trainee tests', () => {
 		});
 	});
 
+	it('update days worked for trainee', (done) =>{
+		chai.request('http://localhost:4000').post('/trainee/daysToWork/' + userId).send({'trainee_days_worked': '10'}).end((err, res) => {
+			if (res.status == '200'){
+				console.log(res.body);
+				done();
+			}
+			else if (res.status == '400' || '404'){
+				console.log(res.body);
+			}
+			else {
+				console.log('test failed for unknown reasons');
+			}
+		});
+	});
+
+	it('update trainee details', (done) => {
+		chai.request('http://localhost:4000').post('/trainee/update/' + userId).send(updateUser).end((err, res) => {
+			if (res.status == '200'){
+				console.log(res.body);
+				done();
+			}
+			else if (res.status == '404' || '400'){
+				console.log(res.body);
+			}
+			else {
+				console.log('test failed for unknown reasons');
+			}
+		});
+	});
+	
+	it('update trainee bursary details', (done) => {
+		chai.request('http://localhost:4000').post('/trainee/editBursary/' + userId).send(updateBursary).end((err, res) => {
+			if (res.status == '200'){
+				console.log(res.body);
+				done();
+			}
+			else if (res.status == '404' || '400'){
+				console.log(res.body);
+			}
+			else {
+				console.log('test failed for unknown reasons');
+			}
+		});
+	});
+
+	it('update start and end dates', (done) => {
+		chai.request('http://localhost:4000').post('/trainee/editDates/' + userId).send(updateDates).end((err, res) => {
+			if (res.status == '200'){
+				console.log(res.body);
+				done();
+			}
+			else if (res.status == '404' || '400'){
+				console.log(res.body);
+			}
+			else {
+				console.log('test failed for unknown reasons');
+			}
+		});
+	});
+
 	describe('/send-email', () => {
 		before((done) => {
 			chai.request('http://localhost:4000').post('/trainee/send-email/').send({'trainee_email': userEmail}).end((err, res) => {
@@ -214,6 +309,36 @@ describe('trainee tests', () => {
 					});
 				});
 			});
+		});
+	});
+
+	it('test sort code finder', (done) => {
+		chai.request('http://localhost:4000').post('/trainee/findBank/').send({'sort_code': '090921'}).end((err, res) =>{
+			if (res.status == '200'){
+				console.log(res.body);
+				done();
+			}
+			else if (res.status == '400'){
+				console.log(res.body);
+			}
+			else {
+				console.log('test failed for unknown reasons');
+			}
+		});
+	});
+
+	it('test adding bank', (done) => {
+		chai.request('http://localhost:4000').post('/trainee/addBank/').send({'SortCode': '090921', 'BankName': 'Bank', 'Branch': 'SuperBranch'}).end((err, res) =>{
+			if (res.status == '200'){
+				console.log(res.body);
+				done();
+			}
+			else if (res.status == '205'){
+				console.log(res.body);
+			}
+			else {
+				console.log('test failed for unknown reasons');
+			}
 		});
 	});
 });
